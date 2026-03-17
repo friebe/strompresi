f651ddb23945e5a6bffeeb686fc26b40a727b18e
Jan Friebe <jan.friebe@atos.net>
2026-03-17 14:31:41 +0100

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/strompresi/' : './',
   test: {
     globals: true,
     environment: 'jsdom',
